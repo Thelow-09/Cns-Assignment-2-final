0a96ff275985272b5c0de80b3e37ed2e4d6bc430
Zelalem <zolazola271316@gmail.com>
2023-01-15 17:42:21 +0300

    cns ass2 extesion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,19 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
+import { GoogleComponent } from './google/google.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from '@abacritt/angularx-social-login';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleComponent
   ],
   imports: [
     BrowserModule,
@@ -31,10 +40,33 @@ import {MatDividerModule} from '@angular/material/divider';
     MatTabsModule,
     MatCardModule,
     MatDividerModule,
+    SocialLoginModule
     
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '487318702641-luta9phl20j3rv8nfbjqnhtc2ng78dg8.apps.googleusercontent.com'
+            )
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId')
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  bootstrap: [GoogleComponent]
 })
 export class AppModule { }
  
---
+++ src/app/google/google.component.ts
@@ -0,0 +1,21 @@
+import { SocialAuthService } from '@abacritt/angularx-social-login';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google',
+  templateUrl: './google.component.html',
+  styleUrls: ['./google.component.css']
+})
+export class GoogleComponent {
+  user: any;
+  loggedIn: boolean = false;
+
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+}
